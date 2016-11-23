24b57c2eff85e0a7d819ebda89146ed6ed9df02b
celia albano <albano.celia@gmail.com>
2016-11-23 10:58:19 +0100

    j'avais tout fait peter j'enlève les validations en trop

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -3,13 +3,8 @@ class Crew < ApplicationRecord
 
   has_many :missions, dependent: :destroy
 
-  validates :name, presence: true
-  validates :city, presence: true
-  validates :country, presence: true
-  validates :social_issue, presence: true
-  validates :mission, presence: true
-  validates :description, presence: true, length: { within: 100..500 }
-  validates :values, presence: true
+  validates :email, presence: true
+  validates :description, length: { within: 100..500 }
   validates :website, :allow_blank => true, format: { :with => /((http|ftp)s?:\/\/)?([a-zA-Z0-9<>\.,!@#$%^:;&*()\-_+=]+:[a-zA-Z0-9<>\.,!@#$%^;:&*()\-_+=]+\@)?([a-zA-Z0-9\-\_]+(\/|\.)){2,}[:a-zA-Z0-9\#\?\=\_\.\-\/]*/i, message: "it has to be an url" }
 
 
