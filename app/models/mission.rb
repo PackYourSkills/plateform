59e7c52040ab73d4d41b6b234d80948adbb4d57c
Maxime BDB <maxime@packyourskills.com>
2016-11-24 18:23:08 +0100

    geocode if full adress changed

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -4,10 +4,14 @@ class Mission < ApplicationRecord
 
   # geocode
   geocoded_by :full_address
-  after_validation :geocode, if: :address_changed?
+  after_validation :geocode, if: :full_address_changed?
 
   def full_address
     self.address + ', ' + self.city + ', ' + self.country
+  end
+
+  def full_address_changed?
+    address_changed? || city_changed? || country_changed?
   end
 
 end
